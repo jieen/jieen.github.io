175593f25abde70056f094e849627762addbfe21
luorenyang <luorenyang@gobuylight.com>
2017-03-06 10:20:02 +0800

    add for test

---
+++ js/search.js
@@ -0,0 +1,11 @@
+﻿
+function  secinfo(n)
+{
+for(i=0;i<3;i++)
+document.getElementById("info"+i).className="promenuout";
+document.getElementById("info"+n).className="promenuon";
+for(i=0;i<mainTable.tBodies.length;i++)
+mainTable.tBodies[i].style.display="none";
+mainTable.tBodies[n].style.display="block";
+
+}
